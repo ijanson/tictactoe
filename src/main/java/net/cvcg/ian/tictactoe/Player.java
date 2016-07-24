998bf0daf2444532cc1f00b5a92109f8eb0577ff
Ian <ianjanson@gmail.com>
2016-07-23 18:39:51 -0700

    removed NONE from Player enum

--- src/main/java/net/cvcg/ian/tictactoe/Player.java
+++ src/main/java/net/cvcg/ian/tictactoe/Player.java
@@ -4,5 +4,5 @@ package net.cvcg.ian.tictactoe;
  * Created by ian on 7/23/16.
  */
 public enum Player {
-    NONE, X, Y
+    X, Y
 }
--- src/main/java/net/cvcg/ian/tictactoe/TicTacToeBoard.java
+++ src/main/java/net/cvcg/ian/tictactoe/TicTacToeBoard.java
@@ -1,15 +1,16 @@
 package net.cvcg.ian.tictactoe;
 
+
 /**
  * Created by Ian on 7/23/2016.
  */
 public class TicTacToeBoard {
     private Player[][] board;
     public TicTacToeBoard() {
-        board = new Player[3][3];
+        board = new Player[3][3]; // default initial value is null
     }
     public void move(Player player, int x, int y) {
-        assert player != Player.NONE;
+        assert player != null;
         board[x][y] = player;
     }
     public Player getWinner() {
