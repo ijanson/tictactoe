60cfc32ca739789debcad70cfe732646cded6c86
Ian Janson <ianjanson@gmail.com>
2016-07-23 19:50:30 -0700

    rename winner test to column test add row winner test

--- src/test/java/net/cvcg/ian/tictactoe/TicTacToeBoardTest.java
+++ src/test/java/net/cvcg/ian/tictactoe/TicTacToeBoardTest.java
@@ -14,12 +14,21 @@ public class TicTacToeBoardTest {
     }
 
     @Test
-    public void testGetWinner() {
+    public void testGetWinnerInColumn() {
         TicTacToeBoard board = new TicTacToeBoard();
         board.move(Player.Y, 1, 0);
         board.move(Player.Y, 1, 1);
         board.move(Player.Y, 1, 2);
         assertTrue(board.getWinner() == Player.Y);
+    }
+
+    @Test
+    public void testGetWinnerInRow() {
+        TicTacToeBoard board = new TicTacToeBoard();
+        board.move(Player.X, 0, 2);
+        board.move(Player.X, 1, 2);
+        board.move(Player.X, 2, 2);
+        assertTrue(board.getWinner() == Player.X);
     }
 
 }
