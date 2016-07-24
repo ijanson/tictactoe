935df0aa8bf905a5f622b8e06e225e9b1d61dbad
Ian <ianjanson@gmail.com>
2016-07-23 19:47:37 -0700

    revised getWinner()

--- src/main/java/net/cvcg/ian/tictactoe/TicTacToeBoard.java
+++ src/main/java/net/cvcg/ian/tictactoe/TicTacToeBoard.java
@@ -14,13 +14,13 @@ public class TicTacToeBoard {
         board[x][y] = player;
     }
     public Player getWinner() {
-        checkRow(0);
-        checkRow(1);
-        checkRow(2);
-        checkColumn(0);
-        checkColumn(1);
-        checkColumn(2);
-        return null;
+        Player player = checkRow(0);
+        if (player ==  null) {player = checkRow(1);}
+        if (player ==  null) {player = checkRow(2);}
+        if (player ==  null) {player = checkColumn(0);}
+        if (player ==  null) {player = checkColumn(1);}
+        if (player ==  null) {player = checkColumn(2);}
+        return player;
     }
     public Player[][] getBoard() {
         return board;
@@ -33,8 +33,8 @@ public class TicTacToeBoard {
         else {return null;}
     }
     private Player checkColumn(int column) {
-        if (board[column][0] != null && board[column][0] == board [column][1] && board[column][1] == board [column][1]) {
-            return board[0][column];
+        if (board[column][0] != null && board[column][0] == board [column][1] && board[column][1] == board [column][2]) {
+            return board[column][0];
         }
         else {return null;}
     }
