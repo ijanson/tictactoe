5b23d6b8ff05db48225d4113a3e512cd95f1f552
Ian Janson <ianjanson@gmail.com>
2016-07-23 17:28:29 -0700

    Rename autogenerated classes to start with TicTacToe

--- src/main/java/net/cvcg/ian/tictactoe/TicTacToe.java
+++ src/main/java/net/cvcg/ian/tictactoe/TicTacToe.java
@@ -3,7 +3,7 @@ package net.cvcg.ian.tictactoe;
 /*
  * @author ian, @date 7/23/16 4:56 PM
  */
-public class Library {
+public class TicTacToe {
     public boolean someLibraryMethod() {
         return true;
     }
--- src/test/java/net/cvcg/ian/tictactoe/TicTacToeTest.java
+++ src/test/java/net/cvcg/ian/tictactoe/TicTacToeTest.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.*;
 /*
  * @author ian, @date 7/23/16 4:56 PM
  */
-public class LibraryTest {
+public class TicTacToeTest {
     @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
+        TicTacToe classUnderTest = new TicTacToe();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
